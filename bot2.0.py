ff1b096bdfafd9ec1991b1e8be9efbbf47f060b6
nikitos-net <72700251+nikitos-net@users.noreply.github.com>
2023-05-23 01:38:47 +0300

    Update bot2.0.py

--- bot2.0.py
+++ bot2.0.py
@@ -1,34 +1,36 @@
-﻿import telebot
+import telebot
 import val
 import qrcode
 import parcing
 from telebot import types
 bot = telebot.TeleBot('5724705081:AAF78TW5FNolA_e-t2PhiTR4i_1dHV8Ak0k')
 
-url = ''
-
+def start_button():
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True, one_time_keyboard = True)
+        item = types.KeyboardButton("Начать")
+        keyboard.add(item)
+        return keyboard
+    
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == 'QR':
-        print(url)
-        img = qrcode.make('https://api.glebins.ru/links/a')
-        img.save('C:/Users/Никита/Desktop/qr.png')
-        #bot.send_photo(call.message.chat.id, photo=open('qr.png', 'rb'));
+        bot.send_photo(call.message.chat.id, photo = links[1])
+        
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id,'Добро пожаловать! Я бот для сокращения ссылок.')
-
+    
 @bot.message_handler(content_types=['text'])
 def welcome(message):
-    if message.text == '/start':
+    if message.text == 'Начать':
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True, one_time_keyboard = True)
         item = types.KeyboardButton("Сократить ссылку")
         keyboard.add(item)
         bot.send_message(message.from_user.id, "Нажмите кнопку", reply_markup = keyboard)
         bot.register_next_step_handler(message, start)
     else:
-        bot.send_message(message.from_user.id, 'Напиши /start')
+        bot.send_message(message.from_user.id, 'Нажмите Начать', reply_markup = start_button())
         bot.register_next_step_handler(message, welcome)
 
 @bot.message_handler(content_types=['text'])
@@ -36,17 +38,16 @@ def get_url(message):
     url = message.text
     if val.check_valid_url(url) == True:
         bot.send_message(message.from_user.id, 'Секундочку...')
-        url = parcing.get_cut_url(url)
+        global links
+        links = parcing.get_cut_url(url)
         keyboard = types.InlineKeyboardMarkup()
         item = types.InlineKeyboardButton(text = 'Сгенерировать QR-код', callback_data = 'QR')
         keyboard.add(item)
-        bot.send_message(message.from_user.id, text='Результат: ' + url, reply_markup = keyboard)
+        bot.send_message(message.from_user.id, text='Результат: ' + links[0], reply_markup = keyboard)
+        #bot.send_message(message.from_user.id, 'Нажмите /start')
         bot.register_next_step_handler(message, welcome)
     else:
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True)
-        item = types.KeyboardButton("/start")
-        keyboard.add(item)
-        bot.send_message(message.from_user.id, 'Нерабочая ссылка', reply_markup = keyboard)
+        bot.send_message(message.from_user.id, 'Нерабочая ссылка!', reply_markup = start_button())
         bot.register_next_step_handler(message, welcome)
             
 @bot.message_handler(content_types=['text'])
@@ -56,11 +57,15 @@ def start(message):
         global url
         bot.register_next_step_handler(message, get_url)
     else:
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True, one_time_keyboard = True)
         item = types.KeyboardButton("Сократить ссылку")
         keyboard.add(item)
-        bot.send_message(message.from_user.id, 'Неизвестная команда', reply_markup = keyboard)
+        item = types.KeyboardButton("Сгенерировать QR-код")
+        keyboard.add(item)
+        bot.send_message(message.from_user.id, 'Неизвестная команда!', reply_markup = keyboard)
         bot.register_next_step_handler(message, start)
 
 bot.polling(none_stop=True, interval=0)
+
+
 
